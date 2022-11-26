5648d602e0953a47103cfee336b2075090db3875
Amina Mh <amina.mhadhbi@esprit.tn>
2022-11-26 17:00:45 +0100

    Ajoutez des fichiers projet

---
+++ AM.UI.WEB/Controllers/FlightController.cs
@@ -0,0 +1,166 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO; 
+
+
+namespace AM.UI.WEB.Controllers
+{
+    public class FlightController : Controller
+
+
+    {
+        private readonly IServiceFlight flightservice;
+        private readonly IServicePlane servicePlane;
+
+        public FlightController(IServiceFlight service, IServicePlane service1)
+        {
+            flightservice = service;
+            servicePlane = service1;
+        }
+
+
+        // GET: FlightController
+        public ActionResult Index(DateTime? dateDepart)
+        {
+           
+       
+            if (dateDepart == null) 
+            {
+              
+                return View(flightservice.GetAll().ToList());
+            }
+            else
+            {
+                var dateDepart1 = ((DateTime)dateDepart).ToShortDateString();
+
+                return View(flightservice.GetFlightsByDate((dateDepart1).ToString()).ToList());
+            }
+       
+
+
+        }
+
+        // GET: FlightController/Details/5
+        public ActionResult Details(int id)
+        {
+
+            var flight = flightservice.GetById(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            return View(flight);
+        }
+
+        // GET: FlightController/Create
+        public ActionResult Create()
+        {
+            var Plane = servicePlane.GetAll();
+            ViewBag.PlaneId = new SelectList(Plane, "PlaneId", "PlaneId");
+            return View();
+        }
+
+        // POST: FlightController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Flight flight,IFormFile AirlineImage)
+        {
+            try
+            {
+
+                if(AirlineImage != null)
+                    {
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", AirlineImage.FileName);                
+                
+              Stream stream = new FileStream(path,FileMode.Create);
+                     AirlineImage.CopyTo(stream);
+                    flight.Airline = AirlineImage.FileName;
+
+                }
+                flightservice.Add(flight);
+
+                flightservice.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: FlightController/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flight = flightservice.GetById(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            var Plane = servicePlane.GetAll();
+            ViewBag.PlaneId = new SelectList(Plane, "PlaneId", "PlaneId");
+    
+            return View(flight);
+        }
+
+        // POST: FlightController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Flight flight)
+        {
+            try
+            {
+                flightservice.Update(flight);
+                flightservice.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FlightController/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flight = flightservice.GetById(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return View(flight);
+        }
+
+        // POST: FlightController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var flight = flightservice.GetById(id);
+                flightservice.Delete(flight);
+                flightservice.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AM.UI.WEB/Controllers/TicketController.cs
@@ -0,0 +1,163 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Sockets;
+
+namespace AM.UI.WEB.Controllers
+{
+    public class TicketController : Controller
+    {
+       
+    
+        private readonly IServiceTicket serviceTicket;
+
+        private readonly IServiceFlight serviceFlight;
+
+        private readonly IServicePassenger servicePassenger;
+
+        public TicketController(IServiceTicket service,IServiceFlight service1, IServicePassenger service2)
+        {
+            serviceTicket = service;
+            serviceFlight = service1;
+            servicePassenger = service2;
+
+        }
+
+
+        // GET: TicketController
+        public ActionResult Index()
+        {
+            return View(serviceTicket.GetAll().ToList());
+        }
+
+        // GET: TicketController/Details/5
+        public ActionResult Details(int id, int FlightId, int TicketNbre)
+        {
+
+         //  var Ticket = serviceTicket.GetById(id);
+            if ((id == null) || (FlightId == null))
+              
+            {
+                return NotFound();
+            }
+            var Ticket = serviceTicket.GetAll().FirstOrDefault(m => m.PassengerFK == id && m.FlightFK == FlightId && m.TicketNbre == TicketNbre);
+
+
+            return View(Ticket);
+        }
+
+        // GET: TicketController/Create
+        public ActionResult Create()
+        {
+            var Passenger = servicePassenger.GetAll();
+            var Flight = serviceFlight.GetAll();
+            ViewBag.FlightFK = new SelectList(Flight, "FlightId", "FlightId");
+           ViewBag.PassengerFK = new SelectList(Passenger, "Id", "Id");
+            
+            return View();
+        }
+
+        // POST: TicketController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Ticket Ticket)
+        {
+            try
+            {
+                serviceTicket.Add(Ticket);
+
+                serviceTicket.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: TicketController/Edit/5
+        public ActionResult Edit(int? id,int? FlightId ,int? TicketNbre )
+        {
+            if ((id == null)|| (FlightId == null))
+            {
+                return NotFound();
+            }
+
+            var Ticket = serviceTicket.GetAll().FirstOrDefault(m=>m.PassengerFK == id && m.FlightFK == FlightId && m.TicketNbre == TicketNbre);
+         
+            
+            if (Ticket == null)
+            {
+                return NotFound();
+            }
+           /* 
+            var Passenger = servicePassenger.GetAll();
+            var Flight = serviceFlight.GetAll();
+            ViewBag.FlightFK = new SelectList(Flight, "FlightId", "FlightId");
+            ViewBag.PassengerFK = new SelectList(Passenger, "Id", "Id");*/
+            return View();
+        }
+
+        // POST: TicketController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Ticket ticket)
+        {
+            try
+            {
+
+                Ticket tick = serviceTicket.GetMany(m => m.PassengerFK == ticket.PassengerFK && m.FlightFK == ticket.FlightFK && m.TicketNbre == ticket.TicketNbre).FirstOrDefault();
+                tick.Prix = ticket.Prix;
+                tick.Siege = ticket.Siege;
+                tick.VIP = ticket.VIP;
+                serviceTicket.Update(ticket);
+                serviceTicket.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TicketController/Delete/5
+        public ActionResult Delete(int? id, int? FlightId, int? TicketNbre)
+        {
+            if ((id == null) || (FlightId == null))
+            {
+                return NotFound();
+            }
+
+            var Ticket = serviceTicket.GetAll().FirstOrDefault(m => m.PassengerFK == id && m.FlightFK == FlightId && m.TicketNbre == TicketNbre);
+            if (Ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(Ticket);
+        }
+
+        // POST: TicketController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, int FlightId, int TicketNbre)
+        {
+            try
+            {
+                var Ticket = serviceTicket.GetAll().FirstOrDefault(m => m.PassengerFK == id && m.FlightFK == FlightId && m.TicketNbre == TicketNbre);
+
+                serviceTicket.Delete(Ticket);
+                serviceTicket.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
